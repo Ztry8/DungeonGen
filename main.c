117769fc5a447aea98420a92bd47fa6a9c2cf2c8
AslD <122040070+Ztry8@users.noreply.github.com>
2024-06-23 02:37:27 +0600

    Add files via upload

--- main.c
+++ main.c
@@ -2,26 +2,22 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define WIDTH 150
-#define HEIGHT 100
+#define WIDTH 100
+#define HEIGHT 75
 #define FLOOR_PERCENT 45
 
-#define int long
-
-signed main(void) {
+signed main(int argc, char **arg) {
 	srand((unsigned)time(NULL)); int map[WIDTH][HEIGHT];
 	for (int y = 0; y < HEIGHT; y++) { for (int x = 0; x < WIDTH; x++)  map[x][y] = '#'; }
 
 	int px = rand() % WIDTH, py = rand() % HEIGHT, floor_tiles = 0;
 	while ((int)(FLOOR_PERCENT / 100.f * (WIDTH * HEIGHT)) >= floor_tiles) {
 		switch (rand() % 4) {
-		case 0: px++; break;
-		case 1: px--; break;
-		case 2: py++; break;
-		case 3: py--; break;
+		case 0: px+1 >= WIDTH ? px-- : px++; break;
+		case 1: px-1 <= 0 ? px++ : px--; break;
+		case 2: py+1 >= HEIGHT ? py-- : py++; break;
+		case 3: py-1 <= 0 ? py++ : py--; break;
 		}
-		if (px >= WIDTH) px--;
-		if (py >= HEIGHT) py--;
 
 		if (map[px][py] == '#') map[px][py] = '.', floor_tiles++;
 	};
