072472f4a13b1a3fb16f4d57ac6a962f8f2be7a8
AslD <122040070+Ztry8@users.noreply.github.com>
2024-06-23 02:55:28 +0600

    Add files via upload

--- main.c
+++ main.c
@@ -2,28 +2,30 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define WIDTH 150
-#define HEIGHT 100
-#define FLOOR_PERCENT 45
-
 signed main(int argc, char **arg) {
-	srand((unsigned)time(NULL)); int map[WIDTH][HEIGHT];
-	for (int y = 0; y < HEIGHT; y++) { for (int x = 0; x < WIDTH; x++)  map[x][y] = '#'; }
+	int width, height;
+	float floor_percent;
 
-	int px = rand() % WIDTH, py = rand() % HEIGHT, floor_tiles = 0;
-	while ((int)(FLOOR_PERCENT / 100.f * (WIDTH * HEIGHT)) >= floor_tiles) {
+	puts("Enter the width, height, and percentage of floor tiles to all tiles:");
+	scanf("%d%d%f", &width, &height, &floor_percent);
+
+	srand((unsigned)time(NULL)); int map[width][height];
+	for (int y = 0; y < height; y++) { for (int x = 0; x < width; x++)  map[x][y] = '#'; }
+
+	int px = rand() % width, py = rand() % height, floor_tiles = 0;
+	while ((int)(floor_percent / 100.f * (width * height)) >= floor_tiles) {
 		switch (rand() % 4) {
-		case 0: px+1 >= WIDTH ? px-- : px++; break;
+		case 0: px+1 >= width ? px-- : px++; break;
 		case 1: px-1 <= 0 ? px++ : px--; break;
-		case 2: py+1 >= HEIGHT ? py-- : py++; break;
+		case 2: py+1 >= height ? py-- : py++; break;
 		case 3: py-1 <= 0 ? py++ : py--; break;
 		}
 
 		if (map[px][py] == '#') map[px][py] = '.', floor_tiles++;
 	};
 
-	for (int y = 0; y < HEIGHT; y++) {
-		for (int x = 0; x < WIDTH; x++) putchar(map[x][y]);
+	for (int y = 0; y < height; y++) {
+		for (int x = 0; x < width; x++) putchar(map[x][y]);
 		putchar('\n');
 	};
 
