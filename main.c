f5f801fbfc94610cf51817244a7f1e242d049434
AslD <122040070+Ztry8@users.noreply.github.com>
2024-06-23 22:43:26 +0600

    Added corridors and rooms.

--- main.c
+++ main.c
@@ -7,29 +7,24 @@ signed main(void) {
 	int width, height; float floor_percent;
 	puts("Enter the width, height, and percentage of floor tiles to all tiles:");
 	scanf("%ld%ld%f", &width, &height, &floor_percent);
-
 	srand((unsigned)time(NULL)); int* map = malloc((width * height) * sizeof(int));
 	if (map == NULL) return 1;
 
 	for (int i = 0; i < height * width; i++) map[i] = '#';
-	int px = rand() % width, py = rand() % height, floor_tiles = 0;
-
+	int px = rand() % width, py = rand() % height, floor_tiles = 0, tunnel_length = 0, tunnel_dir = 0;
 	while ((int)(floor_percent / 100.f * (width * height)) >= floor_tiles) {
-		switch (rand() % 4) {
+		switch (tunnel_length ? tunnel_dir : rand() % 5) {
 		case 0: px + 1 >= width ? px-- : px++; break;
 		case 1: px - 1 <= 0 ? px++ : px--; break;
 		case 2: py + 1 >= height ? py-- : py++; break;
 		case 3: py - 1 <= 0 ? py++ : py--; break;
+		case 4: tunnel_dir = rand() % 4; tunnel_length = rand()%(width/100); break;
 		}
 
+		if (tunnel_length) tunnel_length--;
 		if (map[py * width + px] == '#') map[py * width + px] = '.', floor_tiles++;
 	};
 
-	for (int y = 0; y < height; y++) {
-		for (int x = 0; x < width; x++) putchar(map[y * width + x]);
-		putchar('\n');
-	};
-
-	free(map);
-	return 0;
+	for (int y = 0; y < height; y++) { for (int x = 0; x < width; x++) putchar(map[y * width + x]); putchar('\n'); };
+	free(map); return 0;
 }
