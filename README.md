b9e1ea718a9bdfc51e189b038449834ea79fb7f4
AsLD <xtry8@mail.ru>
2024-06-28 20:02:28 +0500

    Update generator

--- README.md
+++ README.md
@@ -7,6 +7,12 @@
 The project has only one file, so you can build it with a single command:   
 ```clang -std=c99 -Wall -Wextra -pedantic main.c -o generator```
 
+#### Designation
+`<` - exit
+`E` - enemy
+`$` - money
+`*` - chest
+
 #### Input data
 Before running the generation, you need to provide the program `scale` argument.   
 For example:
@@ -22,4 +28,4 @@ Scale = 25
 - [x] Added the input for scale. 
 - [x] Replaced stack with the heap.
 - [x] Added corridors and rooms.
-- [ ] Add ```<; >; $; *;``` tiles.
+- [x] Add ```<; E; $; *;``` tiles.
--- main.c
+++ main.c
@@ -10,20 +10,24 @@ signed main(void) {
 
 	for (int i = 0; i < height * width; i++) map[i] = '#';
 	int px = rand() % width, py = rand() % height, floor_tiles = 0, tunnel_length = 0, tunnel_dir = 0;
+	map[py * width + px] = '<';
 	while ((width * height)/3 >= floor_tiles) {
 		switch (tunnel_length ? tunnel_dir : rand() % 5) {
 		case 0: px + 1 >= width ? px-- : px++; break;
 		case 1: px - 1 <= 0 ? px++ : px--; break;
 		case 2: py + 1 >= height ? py-- : py++; break;
 		case 3: py - 1 <= 0 ? py++ : py--; break;
-		case 4: tunnel_dir = rand() % 4; 
-			tunnel_length = rand()%((tunnel_dir <= 1 ? width : height) / 5); 
-			break;
+		case 4: tunnel_dir = rand() % 4; tunnel_length = rand()%((tunnel_dir <= 1 ? width : height) / 5); break;
 		}
 
 		if (tunnel_length) tunnel_length--;
-		if (map[py * width + px] == '#') map[py * width + px] = '.', floor_tiles++;
-	};
+		if (map[py * width + px] == '#') {
+			if (rand()%25) map[py * width + px] = '.';
+			else if (rand()%3) map[py * width + px] = 'E';
+			else map[py * width + px] = (rand() % 2 ? '*' : '$');
+			floor_tiles++;
+		}
+	}; 
 
 	for (int y = 0; y < height; y++) { for (int x = 0; x < width; x++) putchar(map[y * width + x]); putchar('\n'); };
 	free(map); return 0;
