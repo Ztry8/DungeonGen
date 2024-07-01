ab3fcd742400aed240dd826ae529c850a145f4e7
AsLD <xtry8@mail.ru>
2024-07-01 20:40:22 +0500

    General Update

--- README.md
+++ README.md
@@ -1,42 +1,40 @@
 # DungeonGen
-## Dungeon generator for roguelikes created with less than 35 lines of code!
-### This code is written in pure C99. Used the stdio to display a map.
+## Dungeon generator for roguelikes written in pure C99! 
 ![screenshot](https://github.com/Ztry8/DungeonGen/blob/main/screenshots/1.png)
 
-#### Building
+### Building
 The project has only one file, so you can build it with a single command:   
 ```clang -std=c99 -Wall -Wextra -pedantic main.c -o generator```
 
-#### Time
+### Designation
+`<` - exit   
+`E` - enemy   
+`$` - money   
+`*` - chest
+
+### Input data
+Before running the generation, you need to provide the program `scale` argument.   
+For example:
+
+Scale = 10 or 40x30 size:
+![screenshot](https://github.com/Ztry8/DungeonGen/blob/main/screenshots/10s.png)
+
+Scale = 25 or 100x75 size:
+![screenshot](https://github.com/Ztry8/DungeonGen/blob/main/screenshots/25s.png)
+
+### Time
 The time to generate a map with a scale of 200 (600x400 tiles) is: 
 ```
 real	0m0.767s
 user	0m0.022s
 sys	0m0.006s
 ```
-If the program was built by the command in the previous section.
+If the program was built by the command in the `Building` section.
 
-#### Designation
-`<` - exit   
-`E` - enemy   
-`$` - money   
-`*` - chest
-`+` - door
-
-#### Input data
-Before running the generation, you need to provide the program `scale` argument.   
-Scale = 1 => 4x3 size.
-For example:
-
-Scale = 10   
-![screenshot](https://github.com/Ztry8/DungeonGen/blob/main/screenshots/10s.png)
-
-Scale = 25   
-![screenshot](https://github.com/Ztry8/DungeonGen/blob/main/screenshots/25s.png)
-
-#### TODO
+### TODO
 - [x] Fixed the bug with generating a map outside the boundary.
 - [x] Added the input for scale. 
 - [x] Replaced stack with the heap.
 - [x] Added corridors and rooms.
-- [x] Add ```<; E; $; *; +;``` tiles.
+- [x] Added ```<; E; $; *;``` tiles.
+- [x] Optimizate for large areas.
--- main.c
+++ main.c
@@ -2,34 +2,46 @@
 #include <time.h>
 #include <stdlib.h>
 #define int long 
+
+int min(int a, int b) { return a > b ? b : a; }
+
 signed main(void) {
 	int scale; puts("Enter the scale:"); scanf("%ld", &scale); 
-	if (scale < 3) { puts("Scale must be more than 3!"); return 1; }
+	if (scale < 10) { 
+		puts("Scale must be more than 10!"); 
+		return 1; 
+	}
+
 	int width = 3 * scale, height = 2 * scale; srand((unsigned)time(NULL)); 
 	int* map = malloc((width * height) * sizeof(int)); if (map == NULL) return 1;
-
 	for (int i = 0; i < height * width; i++) map[i] = '#';
-	int px = rand() % width, py = rand() % height, floor_tiles = 0, tunnel_length = 0, tunnel_dir = 0;
+	int px = rand() % width, py = rand() % height, floor_tiles = width * height/3, tunnel_length = 0, tunnel_dir = 0; 
 	map[py * width + px] = '<';
-	while ((width * height)/3 >= floor_tiles) {
+
+	while (floor_tiles--) {
 		switch (tunnel_length ? tunnel_dir : rand() % 5) {
 		case 0: px + 1 >= width ? px-- : px++; break;
 		case 1: px - 1 <= 0 ? px++ : px--; break;
 		case 2: py + 1 >= height ? py-- : py++; break;
 		case 3: py - 1 <= 0 ? py++ : py--; break;
-		case 4: tunnel_dir = rand() % 4; tunnel_length = rand()%((tunnel_dir <= 1 ? width : height) / 5); break;
-		}
-
-		int index = py * width + px;
+		case 4: tunnel_dir = rand() % 4; tunnel_length = rand()%((tunnel_dir <= 1 ? width : height) / 5); break; 
+		}	
 		if (tunnel_length) tunnel_length--;
-		if (map[index] == '#') {
-			if (rand()%25) map[index] = '.';
-			else if (!rand()%5 && tunnel_length) map[index] = '+';
-			else if (rand()%3) map[index] = 'E';
-			else map[index] = (rand() % 2 ? '*' : '$');
-			floor_tiles++;
+		if (map[py * width + px] == '#') map[py * width + px] = '.';
+	};
+	
+	for (int i = 0; i < scale; i++) { 
+		int room_x = rand()%(int)(width*0.9f), room_y = rand()%(int)(height), room_w = rand()%(width/10), room_h = rand()%(height/10);
+		for (int y = room_y; y < min(room_y+room_h, height); y++) { 
+			for (int x = room_x; x < min(room_x+room_w, width); x++) map[y * width + x] = '.'; 
 		}
-	}; 
-	for (int y = 0; y < height; y++) { for (int x = 0; x < width; x++) putchar(map[y * width + x]); putchar('\n'); };
-	free(map); return 0;
+	}
+
+	for (int y = 0; y < height; y++) { 
+		for (int x = 0; x < width; x++) putchar(map[y * width + x]); 
+		putchar('\n'); 
+	}
+
+	free(map); 
+	return 0;
 }
